ee21e8aaff1c8a1d63f840f3d670cf2169229000
alokyadav686 <alokkumaryadav2799@gmail.com>
2024-10-08 23:14:10 +0530

    navigation bar done

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-let s= document.querySelector("p");
-let btn = document.querySelector("#one");
-s.innerText="HELLO";
+// let s= document.querySelector("p");
+// let btn = document.querySelector("#one");
+// s.innerText="HELLO";
 
-let turn=true;
+// let turn=true;
 
 
-btn.addEventListener("click", () =>{
+// btn.addEventListener("click", () =>{
 
     
-    if(turn===true){
-    s.innerText="you are cutie";
-        turn=false;
-}
-else if(turn===false){
-    s.innerText="hello";
-    console.log(turn);
-    turn=true;
-}
-});
+//     if(turn===true){
+//     s.innerText="you are cutie";
+//         turn=false;
+// }
+// else if(turn===false){
+//     s.innerText="hello";
+//     console.log(turn);
+//     turn=true;
+// }
+// });
