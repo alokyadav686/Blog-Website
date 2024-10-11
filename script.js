2f2b29168a271f9a93791625e02eb57f54235be8
alokyadav686 <alokkumaryadav2799@gmail.com>
2024-10-11 23:47:31 +0530

    js added

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ let menubtn = document.querySelector("#menuline");
 
 let uparWala= document.querySelector(".cntt");
 
-menubtn.addEventListener("click", ()=>{
+menubtn.addEventListener("click", (ignoringmenubtn)=>{
 
     console.log("clickedd");
 
     uparWala.classList.remove("hide");
+    ignoringmenubtn .stopPropagation();
 
+
+})
+document.body.addEventListener("mouseover", ()=>{
+    uparWala.classList.add("hide");
 })
